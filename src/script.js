850a26ecc7b3841cdad8e19d9f12615d814f4afb
Kelly Masoto <117077367+kellymasoto@users.noreply.github.com>
2024-03-27 10:02:08 +0200

    script.update

--- src/script.js
+++ src/script.js
@@ -26,6 +26,8 @@ function getWeather(response) {
               />`;
   let iconElement = document.querySelector("#icon");
   iconElement.innerHTML = icon;
+
+  getForecast(response.data.city);
 }
 
 function displayTime(now) {
@@ -63,6 +65,48 @@ function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   searchCity(searchInput.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "6a17ee632b5b60aea453bdfdf4of6te4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+        <div class="weather-forecast-day">
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+  
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>${Math.round(day.temperature.maximum)}°C</strong>
+            </div>
+            <div class="weather-forecast-temperature">${Math.round(
+              day.temperature.minimum
+            )}°C</div>
+          </div>
+        </div>
+      `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector("#search-form");
